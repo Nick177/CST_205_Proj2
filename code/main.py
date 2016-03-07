3b81baf157b325bfbae2e3a76aedb28ee5a3a915
Nicholas R <nicholasirosas@student.hartnell.edu>
2016-03-07 13:15:01 -0800

    Added pixel manipulation functionality to main.py and created manip.py

--- code/main.py
+++ code/main.py
@@ -1,31 +1,75 @@
 from PIL import Image
-import convert
+import myConvert
+import manip
 
 
-#Uncomment multiline comment below when there is a image in folder
-#type picture name in open() function below
-"""
+#The following 2 variables are used to navigate thru the picture's pixels
+# x and y coordinates
+pixX = 0
+pixY = 0
 
-im = image.open(file name goes here)
+im = Image.open('../Pictures/1.png')
 
 width, height = im.size
 
-rgb_im = im.convert('RGB')
-"""
+#Folling line of code makes a copy of image
+#This copy will be used as the changed image
+jm = im.copy()
 
 msg = "hi"
 
 msgLength = len(msg)
 
-#below is a unfinished for loop that might be needed
+#The following variable will be used in inner for loop
+#It will go thru the indexes of the binary string of the current letter
+nextIndex = 0
+
+#below is a for loop that will iterate  thru the individual letters
+#in a message (takes one letter at a time)
 for ix in range(0, msgLength):
-	asciNum = convertToAsci(msg[ix])
-	letterBin = convertToBinary(asciNum)
-	letterLength = len(letterBin)
+	asciOfLetter = convertToAsci(msg[ix])
+	letterBin = convertToBinary(asciOfLetter)
+	letterBin = checkEight(letterBin)
 	
-	for jx in range(0, letterLength):
+	#This for loop will run 3 times for 3 pixels needed to store a single letter
+	#Essentially, this will encrypt the current letter in our picture
+	for jx in range(0, 3):
+		r, g, b = jm.getpixel((0,0))
+		rBin = convertToBinary(r)
+		gBin = convertToBinary(g)
+		bBin = convertToBinary(b)
+		
+		if nextIndex < 8:
+			rBin =  changeLeastSig(rbin, letterBin[nextIndex])
+			nextIndex = nextIndex + 1
+		if nextIndex < 8:
+			gBin = changeLeastSig(gBin, letterBin[nextIndex])
+			nextIndex = nextIndex + 1
+		if nextIndex < 8:
+			bBin = changeLeastSig(bBin, letterBin[nextIndex])
+			nextIndex = nextIndex + 1
+		
+		r = convertToDecimal(rBin)
+		g = converToDecimal(gBin)
+		b = converToDecimal(bBin)
+
+		jm.putpixel((jx + pixX), 0 + pixY), (r, g, b))
+	
+	
+	if(pixX <= width)
+		pixX = pixX + 3
+	else
+		pixX = 0
+		pixY = pixY + 1
+	
+	#reassign nextIndex to 0 to be ready for next binary string of next letter
+	nextIndex = 0
 		
 
+		
+
+	
+	
 
 #r, g, b = rgb_im.getPixel((x, y))
 
@@ -33,7 +77,10 @@ for ix in range(0, msgLength):
 
 
 
+#Summary: returns the binary number needed with all 0
+lengthx=len(convertToBinary(10))
 
 
+jm.save('newPic', 'PNG')
 
 
---
+++ code/manip.py
@@ -0,0 +1,13 @@
+def checkEight (x):
+	lengthx = len(x)
+	neededZeros=8-lengthx
+	for i in range (0, neededZeros):
+		result=result+'0'
+
+    result=result+x
+    return result
+
+"""
+def changeLeastSig(binaryColor, binaryLetter)
+
+"""
